e5559734da48db3bbfb603a4d8f5f5853b9be667
Alonso Vazquez Tena <145672461+alonsovazqueztena@users.noreply.github.com>
2024-12-15 22:58:51 -0700

    Milestone 5 Update

--- app.h
+++ app.h
@@ -1,6 +1,6 @@
 // Alonso Vazquez Tena
-// December 1, 2024
-// Milestone 4: Embedded Application Release 3
+// December 15, 2024
+// Milestone 5: Embedded Application Release 4
 // This is my own work.
 
 // We are defining the app header file here to ensure we declare the necessary functions.
